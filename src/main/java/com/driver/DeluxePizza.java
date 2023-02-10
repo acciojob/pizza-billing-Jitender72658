843b0e9a9207bb899bdfb8fd9147da30517907f8
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-10 19:23:26 +0530

    Submitting Assignment

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,12 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+
+     int price;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        addExtraCheese();
+        addExtraToppings();
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,69 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int cheeseCount;
+    private int toppingCount;
+    private int bagCount;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg) this.price = 300;
+        else this.price = 400;
     }
 
     public int getPrice(){
+        if(isVeg){
+            price = 300;
+        }
+        else{
+            price = 400;
+        }
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        cheeseCount++;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+            toppingCount++;
     }
 
     public void addTakeaway(){
-        // your code goes here
+        bagCount++;
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(isVeg) {
+            System.out.println("Veg pizza base price = "+this.price);
+            bill="300";
+            if(cheeseCount>0){
+                System.out.println("Extra Cheese Added: "+80*cheeseCount);
+                bill = String.valueOf((Integer.parseInt(bill) + 80*cheeseCount));
+            }
+            if(toppingCount>0){
+                bill = String.valueOf((Integer.parseInt(bill) + 70*toppingCount));
+                System.out.println("Extra Toppings Added: "+70*toppingCount);
+            }
+        }
+        else {
+            System.out.println("Non-veg pizza base price = "+this.price);
+            bill="400";
+            if(cheeseCount>0){
+                System.out.println("Extra Cheese Added: "+80*cheeseCount);
+                bill = String.valueOf((Integer.parseInt(bill) + 80));
+            }
+            if(toppingCount>0){
+                bill = String.valueOf((Integer.parseInt(bill) + 120*toppingCount));
+                System.out.println("Extra Toppings Added: "+120*toppingCount);
+            }
+        }
+        if(bagCount>0){
+            bill = String.valueOf((Integer.parseInt(bill) + 20*bagCount));
+            System.out.println("Paperbag Added: "+20*bagCount);
+        }
+        return "Total Price: "+this.bill;
     }
 }
